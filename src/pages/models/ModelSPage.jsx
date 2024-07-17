f3fa7763c22df65ff3add2176d036b02cf717e9f
Marco Alayn <marc_rob92@hotmail.com>
2024-07-17 14:57:01 -0700

    model x page

--- src/pages/models/ModelSPage.jsx
+++ src/pages/models/ModelSPage.jsx
@@ -5,53 +5,58 @@ import imageBackgroundHero from "../../assets/images/model_s.avif";
 import imageSpecificationsHero from "../../assets/images/Model-S-Specs-Hero-Desktop-LHD.avif";
 
 //gris
-import firstSG from "../../assets/images/model_x_paintings/Stealth Grey/01.jfif";
-import secondSG from "../../assets/images/model_x_paintings/Stealth Grey/02.jfif";
-import thirdSG from "../../assets/images/model_x_paintings/Stealth Grey/03.jfif";
-import fourthSG from "../../assets/images/model_x_paintings/Stealth Grey/04.jfif";
-import blackSG from "../../assets/images/model_x_paintings/Stealth Grey/interiors/black.jfif";
-import whiteSG from "../../assets/images/model_x_paintings/Stealth Grey/interiors/white.jfif";
+import firstSG from "../../assets/images/model_s_paintings/Stealth Grey/01.jfif";
+import secondSG from "../../assets/images/model_s_paintings/Stealth Grey/02.jfif";
+import thirdSG from "../../assets/images/model_s_paintings/Stealth Grey/03.jfif";
+import fourthSG from "../../assets/images/model_s_paintings/Stealth Grey/04.jfif";
+import blackSG from "../../assets/images/model_s_paintings/Stealth Grey/interiors/black.jfif";
+import whiteSG from "../../assets/images/model_s_paintings/Stealth Grey/interiors/white.jfif";
+import creamSG from "../../assets/images/model_s_paintings/Stealth Grey/interiors/cream.jfif";
 
 // blanco perla
-import firstWp from "../../assets/images/model_x_paintings/White Pearl/01.jfif";
-import secondWp from "../../assets/images/model_x_paintings/White Pearl/02.jfif";
-import thirdWp from "../../assets/images/model_x_paintings/White Pearl/03.jfif";
-import fourthWp from "../../assets/images/model_x_paintings/White Pearl/04.jfif";
-import blackWp from "../../assets/images/model_x_paintings/White Pearl/interiors/black.jfif";
-import whiteWp from "../../assets/images/model_x_paintings/White Pearl/interiors/white.jfif";
+import firstWp from "../../assets/images/model_s_paintings/White Pearl/01.jfif";
+import secondWp from "../../assets/images/model_s_paintings/White Pearl/02.jfif";
+import thirdWp from "../../assets/images/model_s_paintings/White Pearl/03.jfif";
+import fourthWp from "../../assets/images/model_s_paintings/White Pearl/04.jfif";
+import blackWp from "../../assets/images/model_s_paintings/White Pearl/interiors/black.jfif";
+import whiteWp from "../../assets/images/model_s_paintings/White Pearl/interiors/white.jfif";
+import creamWp from "../../assets/images/model_s_paintings/White Pearl/interiors/cream.jfif";
 
 //deep Metallic blue
-import firstDBM from "../../assets/images/model_x_paintings/Deep Blue Metallic/01.jfif";
-import secondDBM from "../../assets/images/model_x_paintings/Deep Blue Metallic/02.jfif";
-import thirdDBM from "../../assets/images/model_x_paintings/Deep Blue Metallic/03.jfif";
-import fourthDBM from "../../assets/images/model_x_paintings/Deep Blue Metallic/04.jfif";
-import blackDBM from "../../assets/images/model_x_paintings/Deep Blue Metallic/interiors/black.jfif";
-import whiteDBM from "../../assets/images/model_x_paintings/Deep Blue Metallic/interiors/white.jfif";
+import firstDBM from "../../assets/images/model_s_paintings/Deep Blue Metallic/01.jfif";
+import secondDBM from "../../assets/images/model_s_paintings/Deep Blue Metallic/02.jfif";
+import thirdDBM from "../../assets/images/model_s_paintings/Deep Blue Metallic/03.jfif";
+import fourthDBM from "../../assets/images/model_s_paintings/Deep Blue Metallic/04.jfif";
+import blackDBM from "../../assets/images/model_s_paintings/Deep Blue Metallic/interiors/black.jfif";
+import whiteDBM from "../../assets/images/model_s_paintings/Deep Blue Metallic/interiors/white.jfif";
+import creamDBM from "../../assets/images/model_s_paintings/Deep Blue Metallic/interiors/cream.jfif";
 
 //black solid
-import firstBS from "../../assets/images/model_x_paintings/Solid Black/01.jfif";
-import secondBS from "../../assets/images/model_x_paintings/Solid Black/02.jfif";
-import thirdBS from "../../assets/images/model_x_paintings/Solid Black/03.jfif";
-import fourthBS from "../../assets/images/model_x_paintings/Solid Black/04.jfif";
-import blackBS from "../../assets/images/model_x_paintings/Solid Black/interiors/black.jfif";
-import whiteBS from "../../assets/images/model_x_paintings/Solid Black/interiors/white.jfif";
+import firstBS from "../../assets/images/model_s_paintings/Solid Black/01.jfif";
+import secondBS from "../../assets/images/model_s_paintings/Solid Black/02.jfif";
+import thirdBS from "../../assets/images/model_s_paintings/Solid Black/03.jfif";
+import fourthBS from "../../assets/images/model_s_paintings/Solid Black/04.jfif";
+import blackBS from "../../assets/images/model_s_paintings/Solid Black/interiors/black.jfif";
+import whiteBS from "../../assets/images/model_s_paintings/Solid Black/interiors/white.jfif";
+import creamBS from "../../assets/images/model_s_paintings/Solid Black/interiors/cream.jfif";
 
 //ultra red
-import firstUR from "../../assets/images/model_x_paintings/Ultra Red/01.jfif";
-import secondUR from "../../assets/images/model_x_paintings/Ultra Red/02.jfif";
-import thirdUR from "../../assets/images/model_x_paintings/Ultra Red/03.jfif";
-import fourthUR from "../../assets/images/model_x_paintings/Ultra Red/04.jfif";
-import blackUR from "../../assets/images/model_x_paintings/Ultra Red/interiors/black.jfif";
-import whiteUR from "../../assets/images/model_x_paintings/Ultra Red/interiors/white.jfif";
+import firstUR from "../../assets/images/model_s_paintings/Ultra Red/01.jfif";
+import secondUR from "../../assets/images/model_s_paintings/Ultra Red/02.jfif";
+import thirdUR from "../../assets/images/model_s_paintings/Ultra Red/03.jfif";
+import fourthUR from "../../assets/images/model_s_paintings/Ultra Red/04.jfif";
+import blackUR from "../../assets/images/model_s_paintings/Ultra Red/interiors/black.jfif";
+import whiteUR from "../../assets/images/model_s_paintings/Ultra Red/interiors/white.jfif";
+import creamUR from "../../assets/images/model_s_paintings/Ultra Red/interiors/cream.jfif";
 
 //lunar silver
-import firstLS from "../../assets/images/model_x_paintings/Lunar Silver/01.jfif";
-import secondLS from "../../assets/images/model_x_paintings/Lunar Silver/02.jfif";
-import thirdLS from "../../assets/images/model_x_paintings/Lunar Silver/03.jfif";
-import fourthLS from "../../assets/images/model_x_paintings/Lunar Silver/04.jfif";
-import blackLS from "../../assets/images/model_x_paintings/Lunar Silver/interiors/black.jfif";
-import whiteLS from "../../assets/images/model_x_paintings/Lunar Silver/interiors/white.jfif";
-import creamLS from "../../assets/images/model_x_paintings/Lunar Silver/interiors/cream.jfif";
+import firstLS from "../../assets/images/model_s_paintings/Lunar Silver/01.jfif";
+import secondLS from "../../assets/images/model_s_paintings/Lunar Silver/02.jfif";
+import thirdLS from "../../assets/images/model_s_paintings/Lunar Silver/03.jfif";
+import fourthLS from "../../assets/images/model_s_paintings/Lunar Silver/04.jfif";
+import blackLS from "../../assets/images/model_s_paintings/Lunar Silver/interiors/black.jfif";
+import whiteLS from "../../assets/images/model_s_paintings/Lunar Silver/interiors/white.jfif";
+import creamLS from "../../assets/images/model_s_paintings/Lunar Silver/interiors/cream.jfif";
 
 const ModelSPage = () => {
   const modelName = "Model S";
@@ -76,27 +81,27 @@ const ModelSPage = () => {
   const imagesList = {
     stealth_gray: {
       exteriors: [firstSG, secondSG, thirdSG, fourthSG],
-      interiors: [blackSG, whiteSG],
+      interiors: [blackSG, whiteSG, creamSG],
     },
     white_pearl: {
       exteriors: [firstWp, secondWp, thirdWp, fourthWp],
-      interiors: [blackWp, whiteWp],
+      interiors: [blackWp, whiteWp, creamWp],
     },
     deep_Metallic_blue: {
       exteriors: [firstDBM, secondDBM, thirdDBM, fourthDBM],
-      interiors: [blackDBM, whiteDBM],
+      interiors: [blackDBM, whiteDBM, creamDBM],
     },
     solid_black: {
       exteriors: [firstBS, secondBS, thirdBS, fourthBS],
-      interiors: [blackBS, whiteBS],
+      interiors: [blackBS, whiteBS, creamBS],
     },
     ultra_red: {
       exteriors: [firstUR, secondUR, thirdUR, fourthUR],
-      interiors: [blackUR, whiteUR],
+      interiors: [blackUR, whiteUR, creamUR],
     },
     lunar_silver: {
       exteriors: [firstLS, secondLS, thirdLS, fourthLS],
-      interiors: [blackLS, whiteLS,creamLS],
+      interiors: [blackLS, whiteLS, creamLS],
     },
   };
 
